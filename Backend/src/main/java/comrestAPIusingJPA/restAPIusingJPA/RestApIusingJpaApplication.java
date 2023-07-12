8843d4424b68cb3211d154dacdf6e3da2a41d4d1
KARTIKPATIL <91651559+karu3219@users.noreply.github.com>
2023-07-12 18:15:40 +0530

    Add files via upload

---
+++ Backend/src/main/java/comrestAPIusingJPA/restAPIusingJPA/RestApIusingJpaApplication.java
@@ -0,0 +1,13 @@
+package com.restAPIusingJPA.restAPIusingJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApIusingJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApIusingJpaApplication.class, args);
+	}
+
+}
